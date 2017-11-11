d3bd5d59fbf7982fabd3ecc9d8637a61ece010fa
turingF <m15852925655_3@163.com>
2017-11-11 17:59:12 +0800

    20171111

--- tensorflow/lesson6.py
+++ tensorflow/lesson6.py
@@ -1,3 +1,5 @@
+# coding:UTF-8
+#使用mnist集做手写数字识别
 
 
 import tensorflow as tf
@@ -15,13 +17,14 @@ def add_layer(inputs,in_size,out_size,activation_function=None,):
         outputs = activation_function(Wx_plus_b)
     return outputs
 
-
 def compute_accuracy(v_xs,v_ys):
     global prediction
     y_pre = sess.run(prediction,feed_dict={xs:v_xs})
     correct_prediction = tf.equal(tf.argmax(y_pre,1),tf.argmax(v_ys,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
     result = sess.run(accuracy,feed_dict={xs:v_xs,ys:v_ys})
+
+
     return result
 
 #placeholder:
@@ -31,7 +34,7 @@ ys = tf.placeholder(tf.float32,[None,10])
 prediction = add_layer(xs,784,10,activation_function=tf.nn.softmax)
 
 #the error between prediction and real data
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1])) #loss
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
@@ -42,7 +45,7 @@ sess.run(tf.global_variables_initializer())
 
 #important step:
 
-for i in range(1000):
+for i in range(10000):
     batch_xs,batch_ys = mnist.train.next_batch(100)
     sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys})
     if i%50 == 0:
